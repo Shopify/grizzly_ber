b0f757a8b7e494a2fb88f3ab8083453559136773
Ryan Balsdon <ryan.balsdon@shopify.com>
2015-01-08 17:51:49 -0500

    Refactor to properly handle encoded strings without headers

--- lib/grizzly_ber.rb
+++ lib/grizzly_ber.rb
@@ -1,123 +1,40 @@
 require 'grizzly_tag'
 
-class GrizzlyBer
-  attr_reader :value, :tag
+class GrizzlyBerElement
+  attr_reader :tag, :value
 
-  def initialize(hex_string = "")
-    @tag = nil
-    @length = nil
-    @value = nil
-    decode_hex(hex_string)
-  end
-
-  def value=(new_value)
-    if new_value.is_a? String
-      @value = new_value
-      @tag = nil if isConstruct?
-    elsif new_value.is_a? Array and new_value.all? {|tlv| tlv.is_a? GrizzlyBer}
-      @value = new_value
-      @tag = nil if not isConstruct?
-    else
-      @value = nil
-    end
-  end
-
-  def tag=(new_tag)
-    @tag = new_tag
-    @value = nil if isConstruct? and @value.is_a? String
-    @value = nil if not isConstruct? and @value.is_a? Array
-    @value = [] if isConstruct? and @value.nil?
-  end
-
-  def find(tag_or_name)
-    if tag_or_name.is_a? String
-      tag = GrizzlyTag.tag_from_name(tag_or_name)
-    else
-      tag = tag_or_name
-    end
-    return self if @tag == tag
-    return nil unless isConstruct?
-    @value.find{|tlv| tlv.find(tag) != nil}
-  end
-
-  def remove!(tag_or_name)
-    tag_to_remove = find(tag_or_name)
-    @value.delete tag_to_remove unless tag_to_remove.nil?
-  end
-
-  def decode_hex(hex_string)
-    decode_binary([hex_string].pack("H*"))
-  end
-
-  def decode_binary(binary_string)
-    decode_byte_array(binary_string.unpack("C*"))
-  end
-
-  def self.decode_all_hex(hex_string)
-    decode_all_binary([hex_string].pack("H*"))
-  end
-
-  def self.decode_all_binary(binary_string)
-    decode_all_byte_array(binary_string.unpack("C*"))
-  end
-
-  def encode_hex
-    encoded = ""
-    encoded << @tag.to_s(16).upcase
-    if length_of_length == 1
-      encoded << length.to_s(16).rjust(2,'0').upcase
-    else
-      encoded << ((length_of_length-1) | 0x80).to_s(16).rjust(2,'0').upcase
-      encoded << length.to_s(16).rjust((length_of_length-1)*2,'0').upcase
-    end
-    encoded << encode_only_values
-  end
-
-  def encode_binary
-    [encode_hex].pack("H*")
-  end
-
-  def encode_only_values
-    if @value.is_a? String
-      @value.upcase
-    else
-      @value.inject("") {|encoded_children,child| encoded_children << child.encode_hex}
-    end
-  end
-
-  def to_s(indent_size: 0)
-    indent = " " * 3 * indent_size
-    info = GrizzlyTag.tagged(@tag) || {:name => "Unknown Tag", :description => "Unknown"}
-    output  = "#{indent}#{@tag.to_s(16).upcase}: #{info[:name]}\n"
-    output += "#{indent} Description: #{info[:description]}\n"
-    if @value.is_a? Array
-      output += @value.reduce("") { |string, tlv| string += tlv.to_s(indent_size: indent_size+1)}
-    else
-      output += "#{indent} Value: #{@value}"
-      output += ", \"#{[@value].pack("H*")}\"" if info[:format] == :string
-      output += "\n"
-    end
-    output
-  end
-
-  protected
-
-  def length
-    return value.length/2 if value.is_a? String #because hex strings are 2 chars per byte
-    value.inject(0) {|length, tlv| length += tlv.length_of_tag + tlv.length_of_length + tlv.length}
-  end
-
-  def length_of_tag
-    [@tag.to_s(16)].pack("H*").unpack("C*").count
-  end
-
-  def length_of_length
-    1 + ((length < 0x7F) ? 0 : [length.to_s(16)].pack("H*").unpack("C*").count)
-  end
-
-  def decode_byte_array(byte_array)
+  def initialize(byte_array = [])
+    @tag = "" # type is an uppercase hex string
+    @value = nil # type is a byte array if this is a data element or a GrizzlyBer if it's a sequence element
     decode_value decode_length decode_tag byte_array
-    self
+  end
+
+  def tag=(tag)
+    return nil unless tag.is_a? String
+    return nil unless tag.size.even?
+    return nil unless tag =~ /^[0-9A-F]*$/
+    @tag = tag
+  end
+
+  def value=(value)
+    @value = value if value.is_a? Array
+    @value = value if value.is_a? GrizzlyBer
+  end
+
+  def to_ber
+    ber_array = ""
+    ber_array += @tag.upcase
+    value_bytes = @value.pack("C*").unpack("H*").first.upcase if @value.is_a? Array
+    value_bytes = @value.to_ber if @value.is_a? GrizzlyBer
+    if value_bytes.size/2 < 0x7F
+      ber_array << (value_bytes.size/2).to_s(16).rjust(2,'0').upcase
+    else
+      #pack("w") was meant to do this length calc but doesn't work right...
+      number_of_bytes_in_length = ((value_bytes.size/2).to_s(16).size/2.0).ceil
+      ber_array << (number_of_bytes_in_length | 0x80).to_s(16).rjust(2,'0').upcase
+      ber_array += (value_bytes.size/2).to_s(16).rjust(number_of_bytes_in_length*2,'0').upcase
+    end
+    ber_array += value_bytes
   end
 
   private
@@ -127,12 +44,12 @@ class GrizzlyBer
     return [] if byte_array.size < 1
 
     first_byte = byte_array.shift
-    @tag = first_byte
+    @tag << first_byte.to_s(16).rjust(2,'0').upcase
     return byte_array if (first_byte & 0x1F) != 0x1F
 
     while byte_array.size > 0
       next_byte = byte_array.shift
-      @tag = (@tag << 8) | next_byte
+      @tag << next_byte.to_s(16).rjust(2,'0').upcase
       return byte_array if (next_byte & 0x80) != 0x80
     end
 
@@ -157,25 +74,110 @@ class GrizzlyBer
     byte_array
   end
 
-  def isConstruct?
-    ([@tag.to_s(16)].pack("H*").unpack("C*").first & 0x20) == 0x20
-  end
-
   def decode_value(byte_array)
     return [] if byte_array.size < 1 or byte_array.size < @length
-    if isConstruct?
-      @value = []
-      children_bytes = byte_array.shift(@length)
-      @value << GrizzlyBer.new.decode_byte_array(children_bytes) while children_bytes.size > 0
-    else
-      @value = byte_array.shift(@length).pack("C*").unpack("H*").first.upcase
-    end
-  end
-
-  def self.decode_all_byte_array(byte_array)
-    tlv_array = []
-    tlv_array << GrizzlyBer.new.instance_eval { decode_byte_array(byte_array) } while byte_array.size > 0
-    tlv_array
+    return @value = byte_array.shift(@length) if [@tag].pack("H*").unpack("C*").first & 0x20 == 0
+    @value = GrizzlyBer.new.from_ber byte_array.shift(@length)
   end
 end
 
+class GrizzlyBer
+  include Enumerable
+
+  def initialize(hex_string = "")
+    @elements = [] # type is an array of GrizzlyBerElement
+    from_ber_hex_string(hex_string)
+  end
+
+  def from_ber_hex_string(hex_string)
+    self.from_ber [hex_string].pack("H*").unpack("C*")
+  end
+
+  def from_ber(byte_array)
+    while byte_array.size > 0
+      element = GrizzlyBerElement.new(byte_array)
+      return nil if element.tag.nil?
+      return nil if element.value.nil?
+      @elements << element
+    end
+    self
+  end
+
+  def to_ber
+    @elements.reduce("") {|ber_array, element| ber_array += element.to_ber}
+  end
+
+  def [](index_or_tag)
+    return value_of_first_element_with_tag(index_or_tag) if index_or_tag.is_a? String
+    @elements[index_or_tag]
+  end
+
+  def []=(index_or_tag,value)
+    return set_value_for_tag(index_or_tag, value) if index_or_tag.is_a? String
+    @elements[index].value = value
+    @elements[index]
+  end
+
+  def size
+    @elements.size
+  end
+
+  def each &block
+    @elements.each &block
+  end
+
+  def value_of_first_element_with_tag(tag)
+    first_tagged_element = @elements.find {|element| tag.upcase == element.tag}
+    first_tagged_element ||= @elements.find {|element| element.tag == GrizzlyTag.named(tag)[:tag]} if GrizzlyTag.named(tag)
+    first_tagged_element &&= first_tagged_element.value
+  end
+
+  def hex_value_of_first_element_with_tag(tag)
+    first_tagged_element = value_of_first_element_with_tag(tag)
+    first_tagged_element &&= first_tagged_element.pack("C*").unpack("H*").first.upcase
+  end
+
+  def set_value_for_tag(tag, value)
+    tag.upcase!
+    first_tagged_element = @elements.find {|element| tag == element.tag}
+    first_tagged_element ||= @elements.find {|element| element.tag == GrizzlyTag.named(tag)[:tag]} if GrizzlyTag.named(tag)
+    if first_tagged_element.nil?
+      first_tagged_element = GrizzlyBerElement.new
+      first_tagged_element.tag = tag
+      @elements << first_tagged_element
+    end
+    first_tagged_element.value = value
+    first_tagged_element
+  end
+
+  def set_hex_value_for_tag(tag, value)
+    set_value_for_tag(tag, [value].pack("H*").unpack("C*"))
+  end
+
+  def remove!(tag)
+    @elements = @elements.select {|element| element.tag != tag.upcase}
+    self
+  end
+
+  def to_s(indent_size: 0)
+    indent = " " * 3 * indent_size
+    output = ""
+    @elements.each { |element| 
+      info = GrizzlyTag.tagged(element.tag) || {:name => "Unknown Tag", :description => "Unknown"}
+      output  = "#{indent}#{element.tag}: #{info[:name]}\n"
+      output += "#{indent} Description: #{info[:description]}\n"
+      if element.value.is_a? GrizzlyBer
+        output += element.value.reduce("") { |string, tlv| string += element.value.to_s(indent_size: indent_size+1)}
+      else
+        output += "#{indent} Value: #{element.value.pack("C*").unpack("H*").first}"
+        output += ", \"#{element.value.pack("C*")}\"" if info[:format] == :string
+        output += "\n"
+      end
+    }
+    output
+  end
+
+  private
+
+
+end
--- test/test_grizzly_tag.rb
+++ test/test_grizzly_tag.rb
@@ -11,32 +11,32 @@ class GrizzlyTagTest < Minitest::Test
   end
 
   def test_find_by_tag
-    assert_nil GrizzlyTag.tagged(0x00)
-    refute_nil GrizzlyTag.tagged(0x5A)
-    assert_equal GrizzlyTag.tagged(0x5A)[:tag], 0x5a
-    assert_equal GrizzlyTag.tagged(0x5A)[:name], "Application Primary Account Number (PAN)"
-    assert_equal GrizzlyTag.tagged(0x5A)[:description], "Valid cardholder account number"
-    refute_nil GrizzlyTag.tagged(0x9F21)
-    assert_equal GrizzlyTag.tagged(0x9F21)[:tag], 0x9F21
-    assert_equal GrizzlyTag.tagged(0x9F21)[:name], "Transaction Time"
-    assert_equal GrizzlyTag.tagged(0x9F21)[:description], "Local time that the transaction was authorised"
+    assert_nil GrizzlyTag.tagged("00")
+    refute_nil GrizzlyTag.tagged("5A")
+    assert_equal GrizzlyTag.tagged("5A")[:tag], "5A"
+    assert_equal GrizzlyTag.tagged("5A")[:name], "Application Primary Account Number (PAN)"
+    assert_equal GrizzlyTag.tagged("5A")[:description], "Valid cardholder account number"
+    refute_nil GrizzlyTag.tagged("9F21")
+    assert_equal GrizzlyTag.tagged("9F21")[:tag], "9F21"
+    assert_equal GrizzlyTag.tagged("9F21")[:name], "Transaction Time"
+    assert_equal GrizzlyTag.tagged("9F21")[:description], "Local time that the transaction was authorised"
   end
 
   def test_find_by_name
     assert_nil GrizzlyTag.named("")
     refute_nil GrizzlyTag.named("Application Primary Account Number (PAN)")
-    assert_equal GrizzlyTag.named("Application Primary Account Number (PAN)")[:tag], 0x5a
+    assert_equal GrizzlyTag.named("Application Primary Account Number (PAN)")[:tag], "5A"
     assert_equal GrizzlyTag.named("Application Primary Account Number (PAN)")[:name], "Application Primary Account Number (PAN)"
     assert_equal GrizzlyTag.named("Application Primary Account Number (PAN)")[:description], "Valid cardholder account number"
     refute_nil GrizzlyTag.named("Transaction Time")
-    assert_equal GrizzlyTag.named("Transaction Time")[:tag], 0x9F21
+    assert_equal GrizzlyTag.named("Transaction Time")[:tag], "9F21"
     assert_equal GrizzlyTag.named("Transaction Time")[:name], "Transaction Time"
     assert_equal GrizzlyTag.named("Transaction Time")[:description], "Local time that the transaction was authorised"
   end
 
   def test_finding_tag_from_name
     assert_nil GrizzlyTag.tag_from_name("")
-    assert_equal GrizzlyTag.tag_from_name("Transaction Time"), 0x9F21
-    assert_equal GrizzlyTag.tag_from_name("Application Primary Account Number (PAN)"), 0x5A
+    assert_equal GrizzlyTag.tag_from_name("Transaction Time"), "9F21"
+    assert_equal GrizzlyTag.tag_from_name("Application Primary Account Number (PAN)"), "5A"
   end
 end
